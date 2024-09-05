9094c7929432c4c8c4c31d61e807987def156fd6
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-09-05 11:37:17 +0200

    unit test get_ptr_index_kioku

--- test_kioku.c
+++ test_kioku.c
@@ -1,5 +1,6 @@
 #include "kioku.c"
 #include <stdio.h>
+#include <strings.h>
 
 u0 print_block(ramblock *block) {
   ramblock a = {.begin = 0, .end = 0};
@@ -7,7 +8,21 @@ u0 print_block(ramblock *block) {
   printf("ramblock@%p{.begin=%d,.end=%d}\n", block, block->begin, block->end);
 }
 
-u8 ramblock_equals(ramblock *a, ramblock *b) {
+u0 print_status(u8 e) { printf("[%3s] :: ", e ? "OK " : "ERR"); }
+
+u0 test_get_block_kioku(u32 i, ramblock *ptr) {
+  ramblock *r = get_block_kioku(i);
+  print_status(r == ptr);
+  printf("get_block_kioku :: %4d @ %p == %p\n", i, r, ptr);
+}
+
+u0 test_get_ptr_index_kioku(u0 *ptr, u32 index) {
+  u8 i = get_ptr_index_kioku(ptr);
+  print_status(i == index);
+  printf("test_get_ptr_index :: %4d == %4d\n", i, index);
+}
+
+u8 equals_ramblock(ramblock *a, ramblock *b) {
   if (a == b) {
     return 1;
   }
@@ -15,12 +30,6 @@ u8 ramblock_equals(ramblock *a, ramblock *b) {
     return 0;
   }
   return a->begin == b->begin && a->end == b->end;
-}
-
-u0 test_get_block_kioku(u32 i, ramblock *ptr) {
-  ramblock *r = get_block_kioku(i);
-  u8 e = r == ptr;
-  printf("get_block_kioku :: %4d @ %p == %p = %d\n", i, r, ptr, e);
 }
 
 typedef struct {
@@ -36,7 +45,7 @@ u0 test_insert_block_kioku(u32 i, u32 bb, u32 be) {
   insert_block_kioku(i, pile[i]);
   u8 e = 1;
   for (u32 i = 0; i < g_allocated_blocks; i++) {
-    e &= ramblock_equals(get_block_kioku(i), &pile[i]);
+    e &= equals_ramblock(get_block_kioku(i), &pile[i]);
   }
   if (e) {
     return (u0)printf("test_insert_block :: 1\n");
@@ -57,6 +66,9 @@ u0 test_insert_block_kioku(u32 i, u32 bb, u32 be) {
 }
 
 int main(int argc, char *argv[]) {
+
+  printf("\n");
+
   test_get_block_kioku(0, (ramblock *)&g_ram[RAMSIZE - sizeof(ramblock) * 1]);
   test_get_block_kioku(1, (ramblock *)&g_ram[RAMSIZE - sizeof(ramblock) * 2]);
   test_get_block_kioku(2, (ramblock *)&g_ram[RAMSIZE - sizeof(ramblock) * 3]);
@@ -64,15 +76,18 @@ int main(int argc, char *argv[]) {
   test_get_block_kioku(8, (ramblock *)&g_ram[RAMSIZE - sizeof(ramblock) * 9]);
   test_get_block_kioku(6, (ramblock *)&g_ram[RAMSIZE - sizeof(ramblock) * 7]);
   test_get_block_kioku(15, (ramblock *)&g_ram[RAMSIZE - sizeof(ramblock) * 16]);
+
   printf("\n");
 
-  test_insert_block_kioku(0, 1, 2);
-  test_insert_block_kioku(0, 1, 3);
-  test_insert_block_kioku(2, 1, 4);
-  test_insert_block_kioku(2, 1, 4);
-  test_insert_block_kioku(5, 1, 4);
-  test_insert_block_kioku(2, 1, 6);
-  test_insert_block_kioku(3, 1, 7);
-  test_insert_block_kioku(3, 9, 12);
+  test_get_ptr_index_kioku(NULL, 0);
+  test_get_ptr_index_kioku(nullptr, 0);
+  test_get_ptr_index_kioku((u0 *)&g_ram[-128], 0);
+  test_get_ptr_index_kioku((u0 *)&g_ram[RAMSIZE + 128], 0);
+  test_get_ptr_index_kioku((u0 *)&g_ram[1], 1);
+  test_get_ptr_index_kioku((u0 *)&g_ram[128], 128);
+  test_get_ptr_index_kioku((u0 *)&g_ram[200], 200);
+
+  printf("\n");
+
   return 0;
 }
