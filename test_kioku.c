aeb55bf1a5caed9d36f766399071dbfaf3ca7c1d
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-09-07 08:33:27 +0200

    dryizzazione test

--- test_kioku.c
+++ test_kioku.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <strings.h>
 
+u8 equals_ramblock(ramblock *a, ramblock *b) {
+  if (a == b) {
+    return 1;
+  }
+  if (a == nullptr || b == nullptr) {
+    return 0;
+  }
+  return a->begin == b->begin && a->end == b->end;
+}
+
 u0 print_block(ramblock *block) {
   ramblock a = {.begin = 0, .end = 0};
   block = block ? block : &a;
@@ -31,49 +41,6 @@ u0 test_get_ptr_index_kioku(u0 *ptr, u32 index) {
   u8 i = get_ptr_index_kioku(ptr);
   print_status(i == index);
   printf("test_get_ptr_index :: %4d == %4d\n", i, index);
-}
-
-u8 equals_ramblock(ramblock *a, ramblock *b) {
-  if (a == b) {
-    return 1;
-  }
-  if (a == nullptr || b == nullptr) {
-    return 0;
-  }
-  return a->begin == b->begin && a->end == b->end;
-}
-
-typedef struct {
-  ramblock a[1 << 6];
-} ramblock_array;
-
-u0 test_insert_block_kioku2(u32 i, u32 bb, u32 be) {
-  static ramblock pile[1 << 6] = {};
-  for (u32 j = g_allocated_blocks; j > i; i--) {
-    pile[j] = pile[j - 1];
-  }
-  pile[i] = (ramblock){.begin = bb, .end = be};
-  insert_block_kioku(i, pile[i]);
-  u8 e = 1;
-  for (u32 i = 0; i < g_allocated_blocks; i++) {
-    e &= equals_ramblock(get_block_kioku(i), &pile[i]);
-  }
-  if (e) {
-    return (u0)printf("test_insert_block :: 1\n");
-  }
-  printf("test_insert_block :: ERR :: BEGIN\n");
-  for (u32 i = 0; i < g_allocated_blocks; i++) {
-    printf("test_insert_block :: ERR :: ");
-    ramblock *b0 = get_block_kioku(i);
-    ramblock *b1 = &pile[i];
-    printf("{.b = %4d, .e = %4d} ", b0->begin, b1->end);
-    printf("{.b = %4d, .e = %4d} ", b1->begin, b1->end);
-    if (b0->begin != b1->begin || b0->end != b1->end) {
-      printf("<--");
-    }
-    printf("\n");
-  }
-  printf("test_insert_block :: ERR :: END\n");
 }
 
 u0 empty_memory() {
